af24b9b50f8319eb95e15f1dec4c00dd27306a06
Paul Ashour <paul.ashour@gmail.com>
2018-10-08 14:04:43 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -1,6 +1,19 @@
 
 def prog_hash
-  
+ 	{
+    :grace_hopper => {
+      :known_for => "COBOL",
+      :languages => ["COBOL", "FORTRAN"]
+    },
+    :alan_kay => {
+      :known_for => "Object Orientation",
+      :languages => ["Smalltalk", "LISP"]
+    },
+    :dennis_ritchie => {
+      :known_for => "Unix",
+      :languages => ["C"]
+    }
+ }
 
 def hopper
 	programmer_hash = 
