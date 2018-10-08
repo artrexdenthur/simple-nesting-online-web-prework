2ada7813ead72e126f61f4e7864492aec2b94dcf
Paul Ashour <paul.ashour@gmail.com>
2018-10-08 14:05:46 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -1,5 +1,5 @@
 
-def prog_hash
+def create_programmer_hash
  	{
     :grace_hopper => {
       :known_for => "COBOL",
@@ -16,35 +16,21 @@ def prog_hash
   }
 
 def hopper
-	programmer_hash = 
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
-
-
+	programmer_hash = create_programmer_hash
+	programmer_hash[:grace_hopper]
+	
 end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
-	programmer_hash = hopper
+	programmer_hash = create_programmer_hash
 	hopper.fetch(:alan_kay.fetch(:known_for))
 	
 end
 
 def dennis_ritchies_language
-	programmer_hash = hopper
+	programmer_hash = create_programmer_hash
 	hopper.fetch(:dennis_ritchie.fetch(:languages)[0])
 end
 
@@ -55,7 +41,7 @@ def adding_matz
 #    :languages => ["LISP", "C"]
 # }
 
-	programmer_hash = hopper
+	programmer_hash = create_programmer_hash
 	
 	hopper[:yukihiro_matsumoto] = {
 	  known_for: 'Ruby',
@@ -66,7 +52,7 @@ def adding_matz
 end
 
 def changing_alan
-	programmer_hash = hopper
+	programmer_hash = create_programmer_hash
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
@@ -74,20 +60,6 @@ def changing_alan
 end
 
 def adding_to_dennis
-	programmer_hash = 
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
+	programmer_hash = create_programmer_hash
 
 end
